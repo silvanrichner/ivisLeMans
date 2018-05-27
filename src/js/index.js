e912776cbed0a4fb513d48a4a1055fd7a07006cc
Florian Haefliger <florian.haefliger@students.fhnw.ch>
2018-05-27 17:16:17 +0200

    sectorpaths added in javascript

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,45 @@
-var path = anime.path('path');
-// var pathSector2 = sector2.path('path2');
-// var pathSector3 = sector3.path('path3');
+var pathSector1 = anime.path('path#path_sector1');
+var pathSector2 = anime.path('path#path_sector2');
+var pathSector3 = anime.path('path#path_sector3');
 
 var easings = ['linear', 'easeInCubic', 'easeOutCubic', 'easeInOutCubic'];
 
-var motionPath = anime({
+var motionPath1 = anime({
     targets: '.square',
-    translateX: path('x'),
-    translateY: path('y'),
-    rotate: path('angle'),
+    translateX: pathSector1('x'),
+    translateY: pathSector1('y'),
+    rotate: pathSector1('angle'),
     easing: function (el, i) {
         return easings[i];
     },
-    duration: 10000,
+    duration: 5000,
     loop: true
 });
+
+var motionPath2 = anime({
+    targets: '.square',
+    translateX: pathSector2('x'),
+    translateY: pathSector2('y'),
+    rotate: pathSector2('angle'),
+    easing: function (el, i) {
+        return easings[i];
+    },
+    duration: 5000,
+    loop: true
+});
+
+var motionPath3 = anime({
+    targets: '.square',
+    translateX: pathSector3('x'),
+    translateY: pathSector3('y'),
+    rotate: pathSector3('angle'),
+    easing: function (el, i) {
+        return easings[i];
+    },
+    duration: 5000,
+    loop: true
+});
+
+function runAnimation(motionPath1, motionPath2, motionPath3 ) {
+    return motionPath1, motionPath2, motionPath3;
+}
