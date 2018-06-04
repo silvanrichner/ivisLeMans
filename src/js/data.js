bdb2394c3c927db96608affb1d659d09891d7277
Silvan <silvan.richner@gmx.ch>
2018-06-04 09:39:46 +0200

    generating of form and attaching listener

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,61 @@
 $.getJSON("https://raw.githubusercontent.com/silvanrichner/ivisLeMans/master/src/data/data.json", callbackFuncWithData);
 
-function callbackFuncWithData(data)
+var data;
+
+function callbackFuncWithData(json)
 {
+  data = json;
+  buildForm()
+}
+
+function buildForm(){
+  //create a form
+  var f = document.createElement("form");
+  f.id = "form";
+  //f.setAttribute('method',"post");
+  //f.setAttribute('action',"submit.php");
+
   jQuery.each(data, function(i, val) {
-    document.write("<br />" + i + "<br />");
+    //create year section
+    var y = document.createElement("div");
+    var t = document.createTextNode(i);
+    y.appendChild(t);
+    f.appendChild(y);
+
     jQuery.each(this, function(j, val){
-      document.write(this.name + "<br />");
+      var d = document.createElement("div");
+
+      var c = document.createElement("input");
+      c.type = "checkbox";
+      c.id = i + "_" + j;
+      c.name = data[i][j][name];
+      d.appendChild(c);
+
+      var n = document.createTextNode(this.name);
+      d.appendChild(n);
+
+      f.appendChild(d);
     });
-    //$('body').append(document.createTextNode(i));
   });
-  //document.write(JSON.stringify(data["2017"]));
+
+  //create a button
+  var s = document.createElement("input");
+  s.type = "submit";
+  s.value = "Submit";
+  f.appendChild(s);
+
+  // add the form inside the body
+  $("body").append(f);
+
+  //add Listener to form
+  var form = $("#form")[0];
+  if (form.attachEvent) {
+      form.attachEvent("submit", processForm);
+  } else {
+      form.addEventListener("submit", processForm);
+  }
+}
+
+function processForm(){
+  alert("Submitted!")
 }
